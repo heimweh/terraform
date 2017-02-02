6c7f85234eec7221e83d10ff8e99110f6a1a966f
Alexander Hellbom <alexander.hellbom@gmail.com>
2017-02-02 13:18:49 +0100

    Update data source

--- builtin/providers/pagerduty/data_source_pagerduty_vendor.go
+++ builtin/providers/pagerduty/data_source_pagerduty_vendor.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"strings"
 
 	pagerduty "github.com/PagerDuty/go-pagerduty"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -14,14 +15,19 @@ func dataSourcePagerDutyVendor() *schema.Resource {
 		Read: dataSourcePagerDutyVendorRead,
 
 		Schema: map[string]*schema.Schema{
-			"name_regex": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
 			"name": {
-				Type:     schema.TypeString,
-				Computed: true,
+				Type:          schema.TypeString,
+				Optional:      true,
+				ConflictsWith: []string{"name_regex"},
 			},
+
+			"name_regex": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Deprecated:    "Use field 'name' instead",
+				ConflictsWith: []string{"name"},
+			},
+
 			"type": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -33,46 +39,80 @@ func dataSourcePagerDutyVendor() *schema.Resource {
 func dataSourcePagerDutyVendorRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*pagerduty.Client)
 
-	log.Printf("[INFO] Reading PagerDuty vendors")
+	log.Printf("[INFO] Reading PagerDuty vendor")
 
-	resp, err := getVendors(client)
+	var found *pagerduty.Vendor
+	var serviceType string
 
-	if err != nil {
-		return err
-	}
-
-	r := regexp.MustCompile("(?i)" + d.Get("name_regex").(string))
-
-	var vendors []pagerduty.Vendor
-	var vendorNames []string
-
-	for _, v := range resp {
-		if r.MatchString(v.Name) {
-			vendors = append(vendors, v)
-			vendorNames = append(vendorNames, v.Name)
+	// Legacy lookup
+	if _, ok := d.GetOk("name_regex"); ok {
+		resp, err := getVendors(client)
+		if err != nil {
+			return err
 		}
+
+		r := regexp.MustCompile("(?i)" + d.Get("name_regex").(string))
+
+		var vendors []pagerduty.Vendor
+		var vendorNames []string
+
+		for _, vendor := range resp {
+			if r.MatchString(vendor.Name) {
+				vendors = append(vendors, vendor)
+				vendorNames = append(vendorNames, vendor.Name)
+			}
+		}
+
+		if len(vendors) == 0 {
+			return fmt.Errorf("Unable to locate any vendor using the regex string: %s", r.String())
+		} else if len(vendors) > 1 {
+			return fmt.Errorf("Your query returned more than one result using the regex string: %#v. Found vendors: %#v", r.String(), vendorNames)
+		}
+
+		found = &vendors[0]
+
+		serviceType = found.GenericServiceType
+
+	} else if _, ok := d.GetOk("name"); ok {
+		searchName := d.Get("name").(string)
+
+		o := &pagerduty.ListVendorOptions{
+			Query: searchName,
+		}
+
+		resp, err := client.ListVendors(*o)
+		if err != nil {
+			return err
+		}
+
+		for _, vendor := range resp.Vendors {
+			if strings.Contains(strings.ToLower(vendor.Name), strings.ToLower(searchName)) {
+				found = &vendor
+				break
+			}
+		}
+
+		if found == nil {
+			return fmt.Errorf("Unable to locate any vendor with the name: %s", searchName)
+		}
+
+		serviceType = found.GenericServiceType
+	} else {
+		return fmt.Errorf("Either name or name_regex must be specified!")
 	}
-
-	if len(vendors) == 0 {
-		return fmt.Errorf("Unable to locate any vendor using the regex string: %s", r.String())
-	} else if len(vendors) > 1 {
-		return fmt.Errorf("Your query returned more than one result using the regex string: %#v. Found vendors: %#v", r.String(), vendorNames)
-	}
-
-	vendor := vendors[0]
-
-	genericServiceType := vendor.GenericServiceType
 
 	switch {
-	case genericServiceType == "email":
-		genericServiceType = "generic_email_inbound_integration"
-	case genericServiceType == "api":
-		genericServiceType = "generic_events_api_inbound_integration"
+	case serviceType == "email":
+		serviceType = "generic_email_inbound_integration"
+	case serviceType == "api":
+		serviceType = "generic_events_api_inbound_integration"
+	default:
+		return fmt.Errorf("Unknown service type")
 	}
 
-	d.SetId(vendor.ID)
-	d.Set("name", vendor.Name)
-	d.Set("type", genericServiceType)
+	d.SetId(found.ID)
+	d.Set("name", found.Name)
+	d.Set("type", serviceType)
 
 	return nil
 }
